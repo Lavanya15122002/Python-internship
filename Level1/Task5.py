1e8b8cff483c97fdc045c3f3f7ef2a5a89806967
Lavanya15122002 <gatlalavanya777@gmail.com>
2025-10-14 16:05:09 +0530

    Add files via upload

---
+++ Level1/Task5.py
@@ -0,0 +1,9 @@
+def is_palindrome(string):
+    string = string.replace(" ", "").lower()
+    print(string)
+    if string == string[::-1]:
+        print("The string is a palindrome ")
+    else:
+        print("The string is not a palindrome ")
+string = input("Enter a string: ")
+is_palindrome(string)
